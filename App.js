8933955df98950b528baaed00759563cedec1736
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2022-06-09 15:37:50 +0800

    testing 2.0

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing</Text>
+      <Text>testing 2.0</Text>
       <StatusBar style="auto" />
     </View>
   );
