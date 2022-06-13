3b1e0b680ed32da1005a20163dd32326f22bfe74
Executionist <ruifenggoi1234567@gmail.com>
2022-06-13 16:46:30 +0800

    Added ScrollView

--- Components/Page4/Instructions.js
+++ Components/Page4/Instructions.js
@@ -6,6 +6,7 @@ import {
   Image,
   button,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import insStyles from "./InstructionStyle.js";
 
@@ -15,58 +16,60 @@ const onPressTitle = () => {
 
 export default function Ins() {
   return (
-    <View style={insStyles.container}>
-      <Text style={insStyles.baseText}>
-        <Text style={insStyles.titleText} onPress={onPressTitle}>
-          Video Instruction
+    <ScrollView>
+      <View style={insStyles.container}>
+        <Text style={insStyles.baseText}>
+          <Text style={insStyles.titleText} onPress={onPressTitle}>
+            Video Instruction
+          </Text>
+          <Text style={insStyles.firstText}>
+            {"\n"}
+            Before commencing the activity, please bring the camera close to
+            your face so that we can confirm your identity{"\n"}
+          </Text>
         </Text>
-        <Text style={insStyles.firstText}>
+        <View>
+          <Image
+            source={require("./face-scanner.png")}
+            style={{ width: 500, height: 200, borderRadius: 25, padding: 50 }}
+          />
+        </View>
+        <Text style={insStyles.secondText}>
           {"\n"}
-          Before commencing the activity, please bring the camera close to your
-          face so that we can confirm your identity{"\n"}
+          When doing push ups and sit ups, angle the camera as shown in the
+          below pictures. {"\n"} Ensure that full body can be seen so the AI can
+          capture your accurately.{" "}
         </Text>
-      </Text>
-      <View>
-        <Image
-          source={require("./face-scanner.png")}
-          style={{ width: 500, height: 200, borderRadius: 25, padding: 50 }}
-        />
+        <View>
+          <Image
+            source={require("./push-up.png")}
+            style={{
+              width: 500,
+              height: 200,
+              borderRadius: 25,
+              padding: 50,
+              flex: 0.45,
+            }}
+          />
+        </View>
+        <View>
+          <Image
+            source={require("./sit-ups.png")}
+            style={{
+              width: 500,
+              height: 200,
+              borderRadius: 25,
+              padding: 50,
+              flex: 0.45,
+            }}
+          />
+        </View>
+        <View>
+          <TouchableOpacity style={[insStyles.button]}>
+            <Text>Proceed</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <Text style={insStyles.secondText}>
-        {"\n"}
-        When doing push ups and sit ups, angle the camera as shown in the below
-        pictures. {"\n"} Ensure that full body can be seen so the AI can capture
-        your accurately.{" "}
-      </Text>
-      <View>
-        <Image
-          source={require("./push-up.png")}
-          style={{
-            width: 500,
-            height: 200,
-            borderRadius: 25,
-            padding: 50,
-            flex: 0.45,
-          }}
-        />
-      </View>
-      <View>
-        <Image
-          source={require("./sit-ups.png")}
-          style={{
-            width: 500,
-            height: 200,
-            borderRadius: 25,
-            padding: 50,
-            flex: 0.45,
-          }}
-        />
-      </View>
-      <View>
-        <TouchableOpacity style={[insStyles.button]}>
-          <Text>Proceed</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
