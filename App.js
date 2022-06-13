af2e3e54334ac364949d71befd78a517e5d5d3b4
Executionist <ruifenggoi1234567@gmail.com>
2022-06-13 16:42:40 +0800

    Update 2.0

--- App.js
+++ App.js
@@ -3,10 +3,49 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
+<<<<<<< Updated upstream
     <View style={styles.container}>
       <Text>testing 3.2</Text>
       <StatusBar style="auto" />
     </View>
+=======
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Profile") {
+              iconName = focused ? "person-sharp" : "person-outline";
+            }
+
+            if (route.name === "Ippt") {
+              iconName = focused
+                ? "ios-information-circle-sharp"
+                : "ios-information-circle-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "darkgreen",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="Profile"
+          options={{ headerShown: false }}
+          component={ProfileScreen}
+        />
+
+        <Tab.Screen
+          name="Ippt"
+          options={{ headerShown: false }}
+          component={Page3}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+>>>>>>> Stashed changes
   );
 }
 
