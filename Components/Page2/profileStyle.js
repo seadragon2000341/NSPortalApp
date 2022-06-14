d3c2864136b4cc7411183d9b7f27915d48d3a5be
Executionist <ruifenggoi1234567@gmail.com>
2022-06-14 21:46:30 +0800

    Shifted "welcome back"

--- Components/Page2/profileStyle.js
+++ Components/Page2/profileStyle.js
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     backgroundColor: "#fff",
     marginBottom: 10,
+    marginTop: 25,
   },
   titleText: {
     padding: 20,
